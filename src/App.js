ab6f9dc2397be63bfdea085c3c338e0035282681
hionkiong <stellachoi007@gmail.com>
2020-11-15 16:00:58 +0000

    Added GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Hyunkyung Choi and is{" "}
+        <a
+          href="https://github.com/hionkiong/react-weather-practice"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
